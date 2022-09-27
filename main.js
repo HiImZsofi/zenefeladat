65686c5eaf896bdeeb3a522ad8d3c21fe726c52e
HiImZsofi <zsofia.gyori00@gmail.com>
2022-09-27 12:12:28 +0200

    push input to list

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
+var zenek = []
+var zenecime
+var zenehossza
 class Szam {
     constructor(zenecim, zenehossz) {
       this.zenecim = zenecim
       this.zenehossz = zenehossz
     }
   }
+
+$( document ).ready(function() {
+    $("#submit").click(function(){
+        console.log("clicked")
+        zenecime = $("#zenecime").val()
+        zenehossza = $("#zenehossza").val()
+        try {var ujZene = new Szam(zenecime, zenehossza);
+            zenek.push(ujZene);
+            console.log("Uj szam added");}
+        catch (e) {
+            console.log(e)
+        }
+    });
+});
+  
